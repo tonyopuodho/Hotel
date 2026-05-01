6923c9d322c00779248bb2b4219f6bab30865f2c
tonyopuodho <tonyopuodho@gmail.com>
2026-05-01 18:34:56 +0300

    Implementing responsive navigation bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { navigationLinks } from './constants/links'
-import { FaBars } from 'react-icons/fa'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 function Navbar() {
+    const [isOpen,setIsOpen] = useState(false)
   return (
     <div className={`w-full h-[10%] fixed p-2 flex items-center bg-amber-950 font-serif`}>
         <div className={`max-w-7xl w-full md:w-[80%] mx-auto items-center text-white flex justify-between`}>
@@ -16,10 +17,24 @@ function Navbar() {
             }
           </ul>
           <button className={`hidden md:block py-2 px-8 text-xl rounded-sm cursor-pointer text-amber-950 font-bold bg-white`}>Book now</button>
-         <div className={`block md:hidden text-2xl transition-all duration-300 cursor-pointer`}>
-            <FaBars/>
+         <div className={`block md:hidden text-2xl transition-all duration-300 cursor-pointer`} onClick={ () => setIsOpen(!isOpen)}>
+        
+            {
+                isOpen ? <FaTimes/> : <FaBars/>
+            }
          </div>
-        </div>        
+        </div>
+        {/* mobile navigation */}
+        <div className={`absolute md:hidden ${isOpen ? 'left-0': 'left-[-100%]' } transition-all duration-400 h-[100vh] bottom-0 inset-0 w-[80%] bg-amber-950 z-10`}>
+            <ul className={`mt-20 ml-20`}>
+                {
+                    navigationLinks.map((item) => (
+                        <li key={item.id} className={`text-2xl text-white mb-2`}><a href={item.link}>{item.title}</a></li>
+                    ))
+                }
+            </ul>
+            <button className={`w-[90%] ml-4 mt-10 md:block py-2 px-8 text-xl rounded-sm cursor-pointer text-amber-950 font-bold bg-white`}>Book now</button>
+        </div>
     </div>
   )
 }
